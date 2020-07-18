83590a6852d0b51d6ce045e59779861a5b91b16e
Prajjwal <prajjwaldimri@hotmail.com>
2020-07-18 20:56:00 +0530

    Added block code

---
+++ block.test.js
@@ -0,0 +1,33 @@
+const Block = require("./block");
+const { GENESIS_DATA } = require("./config");
+
+describe("Block", () => {
+  const timestamp = "a-date";
+  const lastHash = "foo-hash";
+  const hash = "bar-hash";
+  const data = ["blockchain", "data"];
+  const block = new Block({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+  });
+
+  it("has a timestamp, lasthash, hash and data", () => {
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.data).toEqual(data);
+    expect(block.hash).toEqual(hash);
+    expect(block.lastHash).toEqual(lastHash);
+  });
+
+  describe("genesis()", () => {
+    const genesisBlock = Block.genesis();
+    it("returns a block instance", () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it("returns the genesis data", () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+});
