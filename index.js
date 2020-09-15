b1be1307a1ffb366b2c5060d44bacc15d9757f91
Prajjwal <prajjwaldimri@hotmail.com>
2020-09-15 14:10:53 +0530

    Added redis and pubsub

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new Blockchain();
+const pubSub = new PubSub({ blockchain });
 
 app.use(bodyParser.json());
 
@@ -19,6 +21,15 @@ app.post("/api/mine", (req, res) => {
   res.redirect("/api/blocks");
 });
 
-app.listen(3000, () => {
-  console.log("Listening at PORT:3000");
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Listening at PORT:${PORT}`);
 });
